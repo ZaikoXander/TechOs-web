8b35dd3005e0d680f4bd5bb59b05fec0c0167c76
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2023-09-24 02:05:07 -0300

    feat: Apply styles improvement
    - Add some shadow styles
    - Modify width of content page
    - Add some alt messages for images

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,8 +3,10 @@ import BannerStripe from './BannerStripe'
 
 function Banner() {
   return (
-    <div className="h-60 bg-cover bg-center flex items-center justify-center relative mb-12"
-    style={{ backgroundImage: `url(${BannerImage})` }}>
+    <div
+      className="h-60 bg-cover bg-center flex items-center justify-center relative mb-12"
+      style={{ backgroundImage: `url(${BannerImage})` }}
+    >
       <p className='text-white font-bold text-xl tracking-wider uppercase'>
         Bem vindo a uma das melhores empresas de suporte de tecnologia do país
       </p>
--- src/components/BannerStripe.tsx
+++ src/components/BannerStripe.tsx
@@ -2,7 +2,7 @@ import BannerStripeImage from '../assets/banner-stripe.png'
 
 function BannerStripe() {
   return (
-    <div className='absolute bottom-0 bg-[#A9B3F8] w-full h-4 flex justify-center items-center'>
+    <div className='absolute bottom-0 bg-[#A9B3F8] w-full h-4 flex justify-center items-center filter drop-shadow'>
       <img
         src={BannerStripeImage}
         alt="banner stripe diamond"
