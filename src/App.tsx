e23da1a6edebab09f45f90b1bdbc4246c1bb08b9
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2023-09-24 01:22:17 -0300

    feat: Create Footer component
    - Fix some font weights

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Banner from "./components/Banner"
+import Footer from "./components/Footer"
 
 import whoWeAre from './assets/who-we-are.png'
 import whyChooseOurService from './assets/why-choose-our-service.png'
@@ -9,13 +10,13 @@ function App() {
     <>
       <Header />
       <Banner />
-      <main className='flex flex-col mx-auto gap-20 w-4/5'>
+      <main className='flex flex-col mx-auto gap-20 w-4/5 mb-8'>
         <section className="flex flex-col items-center gap-10">
-          <h1 className='text-3xl'>Quem somos nós?</h1>
+          <h1 className='text-3xl font-bold'>Quem somos nós?</h1>
           <div className='flex items-center gap-6'>
-            <img src={whoWeAre} alt="who we are" width={320} className='shadow' />
-            <div className='w-full h-64 bg-gray-300 rounded-lg'>
-              <p className='text-xl p-4 text-gray-800'>
+            <img src={whoWeAre} alt="who we are" width={320} />
+            <div className='w-full bg-gray-300 rounded-lg'>
+              <p className='text-xl p-4 text-gray-800 shadow-lg'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ullam incidunt molestias nihil vero distinctio earum! Tenetur enim odio molestias, nemo ut amet minus illo eveniet perferendis totam, iusto earum.
                 Quaerat excepturi eaque delectus corrupti nulla soluta sunt, sequi velit porro. Accusantium consequuntur magni culpa architecto officia voluptatem eaque id doloribus numquam! Modi, at? Iste perferendis ipsum animi rerum vitae!
               </p>
@@ -25,9 +26,9 @@ function App() {
         <section className='flex flex-col items-center gap-10'>
           <img src={whyChooseOurService} alt="" width={500} />
           <div className='flex flex-col items-center gap-6'>
-            <h1 className='text-3xl'>Por que contratar nosso serviço?</h1>
-            <div className='w-full h-72 bg-gray-300 rounded-lg'>
-              <p className='text-xl p-4 text-gray-800'>
+            <h1 className='text-3xl font-bold'>Por que contratar nosso serviço?</h1>
+            <div className='w-full bg-gray-300 rounded-lg'>
+              <p className='text-xl p-4 text-gray-800 shadow-lg'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic alias, excepturi qui vitae ad temporibus sit ducimus autem quae neque voluptates rem saepe perspiciatis quibusdam suscipit ratione? Fugiat, impedit expedita!
                 Quis accusamus obcaecati explicabo? Vel repudiandae excepturi dolore fuga, laboriosam, ut molestiae nostrum dignissimos doloribus ipsum veritatis ex voluptas et expedita inventore laborum. Ex, placeat saepe accusamus expedita magnam quis.
                 Cupiditate, doloremque nobis placeat, voluptatibus quisquam modi aliquid neque perspiciatis nisi laudantium ipsa nam sint error deleniti debitis reprehenderit incidunt dolor? Dolore neque esse quas inventore labore, facere quasi porro?
@@ -37,6 +38,7 @@ function App() {
           </div>
         </section>
       </main>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer className='flex justify-around bg-gray-800 text-white py-4 px-10'>
+    <p className='text-lg'>
+      &copy; 2023 TechOS, Todos os direitos reservados.
+    </p>
+    <p className='text-lg'>
+      <strong>Email: </strong>
+      <a href="mailto:techos@gmail.com">techos@gmail.com</a>
+    </p>
+    <p className='text-lg'>
+      <strong>Telefone: </strong>
+      <a href="tel:+5511999999999">(11) 99999-9999</a>
+    </p>
+  </footer>
+  )
+}
+
+export default Footer
