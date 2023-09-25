494da13f81f8b94dea8b79ed1d0c152741ba007a
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2023-09-24 23:31:51 -0300

    Make BannerStripe a private component of Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,17 @@
 import BannerImage from '../assets/banner.png'
-import BannerStripe from './BannerStripe'
+import BannerStripeImage from '../assets/banner-stripe.png'
+
+function BannerStripe() {
+  return (
+    <div className='absolute bottom-0 bg-[#A9B3F8] w-full h-4 flex justify-center items-center filter drop-shadow'>
+      <img
+        src={BannerStripeImage}
+        alt="banner stripe diamond"
+        className='absolute h-16'
+      />
+    </div>
+  )
+}
 
 function Banner() {
   return (
